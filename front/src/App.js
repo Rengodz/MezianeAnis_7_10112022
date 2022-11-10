db45c86d312219500561bf8eb1798a9820ee1831
Anis <mrmezianea@gmail.com>
2022-11-10 21:38:18 +0100

    try

---
+++ front/src/App.js
@@ -0,0 +1,11 @@
+import loginCard from "./components/login/loginCard";
+
+function App() {
+  return (
+    <div>
+      <loginCard></loginCard>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/login/loginCard.jsx
@@ -0,0 +1,5 @@
+function loginCard() {
+    return <h2>blabla</h2>
+}
+
+export default loginCard
